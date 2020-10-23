63d77d135be6659f319ebb19fddfcfae9feba41e
zhuje <59589720+zhuje@users.noreply.github.com>
2020-10-22 21:37:57 -0400

    update nav bar

--- src/components/ManagerComponent.js
+++ src/components/ManagerComponent.js
@@ -9,6 +9,12 @@ import {BrowserRouter, Route, Link} from "react-router-dom";
 
 import SearchComponent from "./SearchComponent";
 import SearchByIDComponent from "./SearchByIDComponent"
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav"
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form'
+import FormControl from 'react-bootstrap/FormControl'
+import HomeComponent from "./HomeComponent";
 
 export class ManagerComponent extends React.Component {
     state = {
@@ -16,20 +22,40 @@ export class ManagerComponent extends React.Component {
     }
     render() {
         return (
+
+            <div>
+
+            <Navbar bg="dark" variant="dark">
+                <Navbar.Brand href="#home"> Movie Match </Navbar.Brand>
+                <Nav className="mr-auto">
+                    <Nav.Link href="/home"> Home </Nav.Link>
+                    <Nav.Link href="/login"> Login </Nav.Link>
+                    <Nav.Link href="/register"> Register </Nav.Link>
+                    <Nav.Link href="/profile"> Profile </Nav.Link>
+                    <Nav.Link href="/search"> Search </Nav.Link>
+
+                </Nav>
+
+            </Navbar>
+            <br />
+
+
             <BrowserRouter>
                 <div className="container">
 
-                    <Link to="/login"> Login </Link> |
-                    <Link to="/register"> Register </Link> |
-                    <Link to="/profile"> Profile </Link> |
-                    <Link to="/table"> Courses </Link> |
-                    <Link to="/grid"> Grid </Link> |
-                    <Link to="/edit"> Editor </Link> |
-                    <Link to={"/search"}> Search </Link>
+                    {/*<Link to="/login"> Login </Link> |*/}
+                    {/*<Link to="/register"> Register </Link> |*/}
+                    {/*<Link to="/profile"> Profile </Link> |*/}
+                    {/*<Link to="/table"> Courses </Link> |*/}
+                    {/*<Link to="/grid"> Grid </Link> |*/}
+                    {/*<Link to="/edit"> Editor </Link> |*/}
+                    {/*<Link to={"/search"}> Search </Link>*/}
 
                     {/*<Route path="/login" exact component={Login}/>*/}
                     {/*<Route path="/register" exact component={Register}/>*/}
                     {/*<Route path="/profile" exact component={Profile}/>*/}
+
+                    <Route path={"/home"} exact component={HomeComponent}/>
 
                     <Route path={"/search"} exact component={SearchComponent}/>
                     <Route path={"/searchByID/:movieID"}
@@ -50,6 +76,10 @@ export class ManagerComponent extends React.Component {
                     {/*    component={CourseEditorComponent}/>*/}
                 </div>
             </BrowserRouter>
+
+
+
+            </div>
         );
     }
 }
--- src/components/SearchByIDComponent.js
+++ src/components/SearchByIDComponent.js
@@ -12,47 +12,45 @@ const SearchByIDComponent = ({ movie={}, movieDetails={}, fetchMovieByID  }) =>
     <div>
 
 
-
         <h1 className={"wdbv-searchByID-header"}> Movie Details </h1>
 
         <div className="row wdbv-searchByID-row">
 
 
-        <div className="col-4">
+            <div className="col-4">
 
 
-            <Card className={"text-center"} style={{ width: '20rem' }}>
-                <Card.Img className={"wbdv-searchByID-card-img-top"} variant="top" src= {movieDetails.Poster} />
-                <Card.Body className={"d-flex flex-column "}>
-                    <Card.Title> {movieDetails.Title} </Card.Title>
-                    <Card.Text>
-                        {movieDetails.Year}
-                    </Card.Text>
-                    <Button className={" align-self-end btn btn-lg btn-block btn-danger "} variant="primary"> Add </Button>
-                </Card.Body>
-            </Card>
+                <Card className={"text-center"} style={{width: '20rem'}}>
+                    <Card.Img className={"wbdv-searchByID-card-img-top"} variant="top"
+                              src={movieDetails.Poster}/>
+                    <Card.Body className={"d-flex flex-column "}>
+                        <Card.Title> {movieDetails.Title} </Card.Title>
+                        <Card.Text>
+                            {movieDetails.Year}
+                        </Card.Text>
+                        <Button className={" align-self-end btn btn-lg btn-block btn-danger "}
+                                variant="primary"> Add </Button>
+                    </Card.Body>
+                </Card>
 
 
+            </div>
+            <div className="col-8">
+                <h1> {movieDetails.Title} </h1>
+                <b> Plot : </b> {movieDetails.Plot} <br/>
+                <b> Rated : </b> {movieDetails.Rated} <br/>
+                <b> Released : </b>{movieDetails.Released} <br/>
+                <b> Runtime : </b>{movieDetails.Runtime} <br/>
+                <b> Genre : </b> {movieDetails.Genre} <br/>
+                <b> Director : </b> {movieDetails.Director} <br/>
+                <b> Writer : </b>{movieDetails.Writer} <br/>
+                <b> Actors : </b> {movieDetails.Actors} <br/>
+                <b> Production : </b> {movieDetails.Production} <br/>
+                <b> imdbRating : </b> {movieDetails.imdbRating} <br/>
+                <b> imdbVotes : </b> {movieDetails.imdbVotes} <br/>
 
+            </div>
         </div>
-        <div className="col-8">
-            <h1> {movieDetails.Title } </h1>
-            <b> Plot :  </b> {movieDetails.Plot } <br/>
-            <b> Rated : </b> {movieDetails.Rated } <br/>
-            <b> Released :  </b>{movieDetails.Released } <br/>
-            <b> Runtime :  </b>{movieDetails.Runtime } <br/>
-            <b> Genre : </b> {movieDetails.Genre } <br/>
-            <b> Director : </b> {movieDetails.Director } <br/>
-            <b> Writer :  </b>{movieDetails.Writer } <br/>
-            <b> Actors : </b> {movieDetails.Actors } <br/>
-            <b> Production : </b> {movieDetails.Production } <br/>
-            <b> imdbRating : </b> {movieDetails.imdbRating } <br/>
-            <b> imdbVotes : </b> {movieDetails.imdbVotes } <br/>
-
-        </div>
-    </div>
-
-
 
 
     </div>
